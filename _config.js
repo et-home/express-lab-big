c717e674a025edfea3b66abad68ac411a78c26d0
et-home <102054479+et-home@users.noreply.github.com>
2022-03-21 13:07:02 -0400

    add _config.js file

---
+++ _config.js
@@ -0,0 +1,13 @@
+const db = {
+  host: "localhost",
+  user: "uername",
+  password: "password",
+  database: "database",
+  connectionLimit: 5,
+};
+
+const config = {};
+
+config.db = db;
+
+module.exports = config;
